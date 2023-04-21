e5a3db48b6aa19d7e38c028579e9870e71893616
Ulisses <silvamunizulisses@hotmail.com>
2023-04-21 15:27:12 -0300

    AUD

--- src/pages/layout/blockly/blocks/ticks/candleInterval.js
+++ src/pages/layout/blockly/blocks/ticks/candleInterval.js
@@ -2,15 +2,15 @@ import config from '../../../../common/const';
 import { translate } from '../../../../../common/i18n';
 
 export default function candleInterval(block) {
-    block
-        .appendDummyInput()
-        .appendField(`${translate('with interval')}:`)
-        .appendField(new Blockly.FieldDropdown(config.candleIntervals), 'CANDLEINTERVAL_LIST');
+  block
+    .appendDummyInput()
+    .appendField(`${translate('with interval')}:`)
+    .appendField(new Blockly.FieldDropdown(config.candleIntervals), 'CANDLEINTERVAL_LIST');
 
-    block.setInputsInline(true);
+  block.setInputsInline(true);
 }
 
 export const getGranularity = block => {
-    const granularity = block.getFieldValue('CANDLEINTERVAL_LIST');
-    return granularity === 'default' ? 'undefined' : granularity;
+  const granularity = block.getFieldValue('CANDLEINTERVAL_LIST');
+  return granularity === 'default' ? 'undefined' : granularity;
 };
