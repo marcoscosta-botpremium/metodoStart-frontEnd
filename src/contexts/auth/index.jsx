e51ddcc5f34a7c629edb71536792736d626f6426
Ulisses <silvamunizulisses@hotmail.com>
2023-08-25 16:57:40 -0300

    fix

--- src/contexts/auth/index.jsx
+++ src/contexts/auth/index.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 import { toast } from 'react-hot-toast';
 import * as api from '../../services/api';
 
